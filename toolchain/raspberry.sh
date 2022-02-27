13f57e2e2e7d12ca1307c72ff3552cf1a1cc6392
Sebastian Ritter <bastie@users.noreply.github.com>
2022-02-27 13:05:31 +0100

    Create raspberry.sh

---
+++ toolchain/raspberry.sh
@@ -0,0 +1,15 @@
+## Install Swift
+# @see https://swift-arm.com/installSwift
+sudo apt update && sudo apt upgrade
+sudo apt install curl
+curl -s https://archive.swiftlang.xyz/install.sh | sudo bash
+sudo apt install swiftlang
+
+# check swift tripple
+swiftc -print-target-info
+
+## Install Git
+sudo apt install git
+
+
+### EOF
