c121d7572c406b97958d3d581ec916eb65ffc58a
Sebastian Ritter <bastie@users.noreply.github.com>
2022-03-25 18:58:08 +0100

    Update raspberry.sh

--- toolchain/raspberry.sh
+++ toolchain/raspberry.sh
@@ -4,8 +4,13 @@
 # @see https://swift-arm.com/installSwift
 sudo apt update && sudo apt upgrade
 sudo apt install curl
+
+# solution 1
 curl -s https://archive.swiftlang.xyz/install.sh | sudo bash
 sudo apt install swiftlang
+
+#solution 2
+curl -s https://packagecloud.io/install/repositories/swift-arm/release/script.deb.sh | sudo bash
 
 # check swift tripple
 swiftc -print-target-info
